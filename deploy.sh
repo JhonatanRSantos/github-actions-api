4f6f11f5fce707f20b99668a8115598c0ba4011c
Jhonatan R. Santos <jhonatanatm@gmail.com>
2020-11-02 23:01:30 -0300

    Corrected CI bugs

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,17 @@ DEPLOY_TYPE=$1
 ENVIRONMENT=$2
 FUNCTION_NAME=$3
 
+TESTING=false
+
 if [ "$DEPLOY_TYPE" == "" ] || [ "$DEPLOY_TYPE" != "lambda" ]; then
   echo "##########################"
   echo "#      DEPLOY ERROR      #"
   echo "##########################"
   echo "   Invalid deploy type    "
   exit
-elif [ "$ENVIRONMENT" == "" ] || 
-  ( [ "$ENVIRONMENT" != "local" ] && 
+elif [ "$ENVIRONMENT" == "" ] ||
+  ( [ "$ENVIRONMENT" != "test" ] &&
+  [ "$ENVIRONMENT" != "local" ] && 
   [ "$ENVIRONMENT" != "debug" ] && 
   [ "$ENVIRONMENT" != "dev" ]  && 
   [ "$ENVIRONMENT" != "prd" ]); then
@@ -23,7 +26,10 @@ elif [ "$ENVIRONMENT" == "" ] ||
   exit
 fi
 
-if [ "$ENVIRONMENT" == "debug" ]; then
+if [ "$ENVIRONMENT" == "debug" ] || [ "$ENVIRONMENT" == "test" ]; then
+  if [ "$ENVIRONMENT" == "test" ]; then
+    TESTING=true
+  fi    
   ENVIRONMENT="local"
 fi
 
@@ -53,8 +59,19 @@ if [ "$FUNCTION_NAME" != "" ]; then
   echo "Deploying function => ${FUNCTION_NAME}"
   yarn serverless deploy -f $FUNCTION_NAME
 
-elif [ "$ENVIRONMENT" == "local" ] || [ "$ENVIRONMENT" == "debug" ]; then
+elif ([ "$ENVIRONMENT" == "local" ] || [ "$ENVIRONMENT" == "debug" ]) &&  ! $TESTING ; then
   yarn serverless offline --stage=local --httpPort=8000
+
+elif $TESTING ; then
+  yarn serverless offline --stage=local --httpPort=8000 &>/dev/null &
+  echo "Sleep for a moment while the project is building with serverless offline"
+  sleep 30
+
+  if [ "$GIT_ACTION" == "" ]; then 
+    yarn newman run ./tests/postman/collection.json -e ./tests/postman/environment.json
+    killall node
+  fi
+
 else
   yarn serverless deploy --stage=${ENVIRONMENT}
   # Destroy all lambdas
